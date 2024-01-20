0d5a015d0bc5b456c7e1bb0b105b27f55db9f69d
Zekii <zach7505@gmail.com>
2024-01-21 01:24:16 +1100

    Finished implementation, and added support for if the port wasnt forwarded already

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 import json
 
@@ -42,21 +43,67 @@ def configure_port_forwarding(data, local_ip):
         # Wait for the page to load
         time.sleep(3)
 
-        # Get the checkbox to remove the previous port forward (there will always be one)
-        checkbox = driver.find_element(By.NAME, "rml")
+        remove_checkbox = None
 
-        current_forwarded_ip = checkbox.get_attribute("value").split("|")[0]
+        try:
+            # Get the checkbox to remove the previous port forward (there will always be one)
+            remove_checkbox = driver.find_element(By.NAME, "rml")
 
-        if current_forwarded_ip == local_ip:
-            print("Server already forwarded")
-            driver.quit()
-            return
+            current_forwarded_ip = remove_checkbox.get_attribute("value").split("|")[0]
 
-        print("Server not forwarded, press enter to proceed with forwarding")
-        input()
+            if current_forwarded_ip == local_ip:
+                print("Server already forwarded")
+                driver.quit()
+                return
 
-        checkbox.click()
-        time.sleep(10)
+        except:
+            pass
+
+        print("Server not forwarded, forwarding now")
+
+        # From here on we need to remove it by clicking it followed by the remove button (if it exists)
+        if remove_checkbox:
+            remove_checkbox.click()
+
+            # Click remove button
+            driver.find_element(By.ID, "sevrtview_remove").click()
+
+        # Wait for page to reload
+        time.sleep(1)
+
+        # Click the add button
+        driver.find_element(By.ID, "sevrtview_add").click()
+
+        # Wait for page to reload
+        time.sleep(1)
+
+        # Find and select the interface in the selection dropdown
+        Select(driver.find_element(By.NAME, "dstWanIf")).select_by_value("ppp2.1")
+
+        # Click to name it as a custom service, we need to click the 2nd one for a custom service
+        driver.find_elements(By.NAME, "radiosrv")[1].click()
+
+        # Name the service. In this case just name it minecraft server
+        driver.find_element(By.NAME, "cusSrvName").send_keys("Minecraft server")
+
+        # Set the server IP to the local IP (the last digits as they designate the device) i.e 192.168.20.xx
+        driver.find_element(By.NAME, "sIp").send_keys(local_ip.split(".")[-1])
+
+        # Enable it in the status dropdown
+        Select(driver.find_element(By.NAME, "status")).select_by_value("1")
+
+        # Set the port start and end numbers
+        driver.find_element(By.NAME, "eStart").send_keys("25565")
+        driver.find_element(By.NAME, "eEnd").send_keys("25565")
+
+        # Select the TCP/UDP option
+        Select(driver.find_element(By.NAME, "proto")).select_by_value("0")
+
+        # Finally click apply and save
+        driver.find_element(By.ID, "natSaveApply").click()
+
+        print("Server forwarded successfully")
+        time.sleep(5)
 
     finally:
         # Close the browser window
